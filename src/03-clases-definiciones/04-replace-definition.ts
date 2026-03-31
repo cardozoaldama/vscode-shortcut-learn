512f18666aae40d2248c0b0ab1b0681b320f685a
Fernando Cardozo <nuevo_10000@outlook.com>
2026-03-31 09:45:58 -0300

    Learn about replacing definitions globally

--- src/03-clases-definiciones/04-replace-definition.ts
+++ src/03-clases-definiciones/04-replace-definition.ts
@@ -1,4 +1,4 @@
-import { SuperHero } from './extra/classes';
+import { Heroe } from './extra/classes';
 
 //* 
 //*     Objetivo:
@@ -11,9 +11,9 @@ import { SuperHero } from './extra/classes';
 //* 
 
 
-const wolverine = new SuperHero();
-const ironman   = new SuperHero();
-const spiderman = new SuperHero();
+const wolverine = new Heroe();
+const ironman   = new Heroe();
+const spiderman = new Heroe();
 
 function greet() {
     return 'El SuperHero Wolverine es genial!';
